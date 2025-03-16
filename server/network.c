7574ea17e9a303db1a36d80bde991ad310571e62
Timothy Riley <timothyjohnriley@gmail.com>
2025-03-16 18:15:35 -0400

    Establish connection for localhost for now

--- server/network.c
+++ server/network.c
@@ -1,38 +1,78 @@
 
 
+#include <errno.h>
+#include <stdbool.h>
 #include <stdio.h>
+
+#if defined(WIN32)
 #include <winsock2.h>
+#else
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#endif
 
 int ip_socket;
+
+void string_to_socket_addr(const char *str_addr, struct sockaddr *sadr) {
+    struct hostent *h;
+
+    memset(sadr, 0, sizeof(*sadr));
+    ((struct sockaddr_in *)sadr)->sin_family = AF_INET;
+
+    ((struct sockaddr_in *)sadr)->sin_port = 0;
+
+    if (str_addr[0] >= '0' && str_addr[0] <= '9') {
+        // it's an ip4 address
+        *(int *)&((struct sockaddr_in *)sadr)->sin_addr = inet_addr(str_addr);
+    }
+    else {
+        // it's a string address
+        if (!(h = gethostbyname(str_addr)))
+            return;
+        *(int *)&((struct sockaddr_in *)sadr)->sin_addr = *(int *)h->h_addr_list[0];
+    }
+
+    return;
+}
 
 // attempt to open network connection
 int network_ip_socket(char* ip_addr, int port) {
 
     struct sockaddr_in address;
-    address.sin_addr.s_addr = ip_addr;
-    address.sin_port = port;
+
+    string_to_socket_addr(ip_addr, (struct sockaddr*)&address);
+
+    //address.sin_addr.s_addr = ip_addr;
+    address.sin_port = htons((short)port);
     address.sin_family = AF_INET;
 
-    int new_socket = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
-
-    if(new_socket == INVALID_SOCKET) {
-        printf("unable to open socket\n");
+    int new_socket = -1;
+    bool _true = true;
+    int i = 1;
+    
+    if((new_socket = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
+        printf("unable to open socket: %s\n", strerror(errno));
         return 0;
     }
 
-    if(ioctl(new_socket, FIONBIO) == -1) {
-        printf("can't make socket non-blocking\n");
+    if(ioctl(new_socket, FIONBIO, &_true) == -1) {
+        printf("can't make socket non-blocking: %s\n", strerror(errno));
         return 0;
     }
 
-    if(setsockopt(new_socket, SOL_SOCKET, SO_BROADCAST, SO_DEBUG, 1) == -1) {
-        printf("can't make socket broadcastable\n");
+    if(setsockopt(new_socket, SOL_SOCKET, SO_BROADCAST, (char*)&i, sizeof(i)) == -1) {
+        printf("can't make socket broadcastable: %s\n", strerror(errno));
         return 0;
     }
 
 
     if(bind(new_socket, (void*)&address, sizeof(address)) == -1) {
-        printf("couldn't bind address and port\n");
+        printf("couldn't bind address and port: %s\n", strerror(errno));
         close(new_socket);
         return 0;
     }
