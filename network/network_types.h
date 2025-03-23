184fcad40f8ebe8f539731d6b21c6b8febccc11e
Timothy Riley <timothyjohnriley@gmail.com>
2025-03-22 22:33:40 -0400

    Start message handling

--- network/network_types.h
+++ network/network_types.h
@@ -2,12 +2,24 @@
 #ifndef NETWORK_TYPES_H
 #define NETWORK_TYPES_H
 
+#include <stdbool.h>
+
 typedef unsigned char byte;
+
+#define MAX_STRING_CHARS 1024
 
 typedef struct {
     byte ip[4];
     unsigned short port;
 } netadr_t;
+
+typedef struct {
+    int cursize;
+    int readcount;
+    int bit;
+    bool oob;
+    byte* data;
+} msg_t;
 
 typedef enum {
     CLIENT,
--- server/main.c
+++ server/main.c
@@ -1,9 +1,13 @@
 
 
+#include <msg.h>
 #include <network.h>
 
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
+
+#define PACKET_HDR_SIZE 4
 
 typedef struct {
     const char* name;
@@ -12,9 +16,45 @@ typedef struct {
 
 server_t server;
 
-void server_status() {
+// command callbacks
+static void server_status() {
 
 }
+
+static void server_anonymous_packet(netadr_t from, msg_t* msg) {
+
+    if(!strncmp("connect", &msg->data[PACKET_HDR_SIZE], strlen("connect"))) {
+        // a more complex message
+    }
+
+    char* s = msg_read_string_line(msg);
+    char* tokens = strtok(s, ' ');
+
+    if(strcmp(tokens[0], "getstatus")) {
+        server_status();
+    }
+    else if (strcmp(tokens[0], "connect")) {
+        // connect
+    }
+    else if (strcmp(tokens[0], "command")) {
+        // perform a command given the message
+    }
+    else {
+        printf("this packet is bad...\n");
+    }
+
+}
+
+static void server_packet_event(netadr_t from, msg_t* msg) {
+
+    // is it from an anonymous client (i.e. one not connected to us)
+    // starts with 0xffff_ffff
+    if ( msg->cursize >= PACKET_HDR_SIZE && *(int *)msg->data == -1) {
+        server_anonymous_packet(from, msg);
+        return;
+	}
+}
+
 
 static void server_init() {
     server.name = "master blaster";
