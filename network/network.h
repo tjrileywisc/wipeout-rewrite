4ad7cb63d03d62b86c906676ee47cdfaef071ce3
Timothy Riley <timothyjohnriley@gmail.com>
2025-05-12 19:42:10 -0400

    Add docs

--- network/network.h
+++ network/network.h
@@ -15,13 +15,13 @@ bool network_has_ip_socket(void);
 void network_set_ip_socket(int sockfd);
 
 /**
- * Bind to a port for running in a
+ * @brief Bind to a port for running in a
  * server context
  */
 void network_bind_ip(void);
 
 /**
- * Connect to any port for running
+ * @brief Connect to any port for running
  * in a client context
  */
 void network_connect_ip(const char* addr);
@@ -33,7 +33,7 @@ bool network_get_packet();
 void network_send_packet(netsrc_t sock, int length, const void* data, netadr_t dest_net);
 
 /**
- * Send a command to the server or to a client
+ * @brief Send a command to the server or to a client
  */
 void network_send_command(const char* command, netadr_t dest);
 
@@ -41,9 +41,17 @@ void network_process_command(const char* command);
 
 int network_get_msg_queue_size(void);
 
-bool network_get_msg_queue_item(msg_queue_item_t *item);
 /**
- * sleep for a duration, or until we have socket activity
+ * @brief get the first message queue item
+ * 
+ * @param item pointer to the item
+ * 
+ * @return true if there is an item, false otherwise
+ */
+bool network_get_msg_queue_item(msg_queue_item_t *item);
+
+/**
+ * @brief sleep for a duration, or until we have socket activity
  * 
  * @param msec duration in milliseconds
  * @return 0 if network is not available, or 1 if so
