00f61e6494173f01b0b952429ed0cd46b161e67c
Timothy Riley <timothyjohnriley@gmail.com>
2025-06-02 19:03:51 -0400

    Make sure test runs

--- tests/main.c
+++ tests/main.c
@@ -7,6 +7,7 @@
 extern void test_network_get_packet(void **state);
 extern void test_network_get_packet_no_data(void **state);
 extern void test_network_get_local_subnet(void **state);
+extern void network_close_socket_sets_socket_invalid(void **state);
 
 extern void empties_queue_after_process(void **state);
 
@@ -16,6 +17,7 @@ int main(void) {
         cmocka_unit_test(test_network_get_packet),
         cmocka_unit_test(test_network_get_packet_no_data),
         cmocka_unit_test(test_network_get_local_subnet),
+        cmocka_unit_test(network_close_socket_sets_socket_invalid),
 
         // dedicated server tests
         //cmocka_unit_test(empties_queue_after_process)
